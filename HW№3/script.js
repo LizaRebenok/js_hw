16f2beed26e5e74c35428bd4921e70976ccf10d5
LizaRebenok <lizabelichenko3@gmail.com>
2024-07-31 21:43:44 +0300

    HW 3  - completed

---
+++ HW№3/script.js
@@ -0,0 +1,9 @@
+`Use strict`
+
+let num1 = +prompt(`Enter a number`);
+let num2 = +prompt(`Enter a other number`);
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
