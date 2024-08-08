b28788fb3d5ff9b2eb4d71b06bd8b2def97e0ac5
LizaRebenok <lizabelichenko3@gmail.com>
2024-08-08 14:24:59 +0300

    HW 7 - completed

--- HW - Hillel/js/script.js
+++ HW - Hillel/js/script.js
@@ -1,1 +1,15 @@
 `Use strict`;
+
+const hoursRequest = Number(prompt(`Введіть кількість годин`));
+const hoursResult = isNaN(hoursRequest) ? Number(prompt(`Помилка! Введіть число.`)) : hoursRequest;
+
+console.log(hoursResult);
+
+const hoursInMinutes = hoursResult * 60;
+console.log(hoursInMinutes);
+
+const minutesToSeconds = hoursInMinutes * 60;
+console.log(minutesToSeconds);
+
+alert(`Результат вашого запиту: ${hoursResult} годин(а) в секундах буде ${minutesToSeconds}.`);
+
