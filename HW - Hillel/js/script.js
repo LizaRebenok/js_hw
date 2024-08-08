bb582810c468a3ce8077b66d8a1ddabf195cd051
LizaRebenok <lizabelichenko3@gmail.com>
2024-08-08 21:44:39 +0300

    HW 8 - completed

--- HW - Hillel/js/script.js
+++ HW - Hillel/js/script.js
@@ -1,1 +1,20 @@
 `Use strict`;
+
+function validNumber(numberRequest) {
+    let number;
+    do {
+        number = Number(prompt(numberRequest));
+        if (isNaN(number)) {
+            alert('Помилка! Введіть число.');
+        }
+    } while (isNaN(number));
+    return number;
+}
+
+const firstNumber = validNumber('Введіть перше число');
+const secondNumber = validNumber('Введіть друге число');
+const thirdNumber = validNumber('Введіть трете число');
+
+const arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(`Середнє арифметичне введених чисел: ${arithmeticMean}.`);
