b1f88e549eb4b1949058b246ca28fe8e5d7a047f
LizaRebenok <lizabelichenko3@gmail.com>
2024-08-09 17:36:00 +0300

    HW 8 - fixed

--- HW - Hillel/js/script.js
+++ HW - Hillel/js/script.js
@@ -1,20 +1,14 @@
 `Use strict`;
 
-function validNumber(numberRequest) {
-    let number;
-    do {
-        number = Number(prompt(numberRequest));
-        if (isNaN(number)) {
-            alert('Помилка! Введіть число.');
-        }
-    } while (isNaN(number));
-    return number;
-}
 
-const firstNumber = validNumber('Введіть перше число');
-const secondNumber = validNumber('Введіть друге число');
-const thirdNumber = validNumber('Введіть трете число');
+const firstNumber = Number(prompt('Введіть перше число'));
+const checkNum1 = isNaN(firstNumber) ? Number(prompt(`Помилка! Введіть число.`)) : firstNumber;
+const secondNumber = Number(prompt('Введіть друге число'));
+const checkNum2 = isNaN(secondNumber) ? Number(prompt(`Помилка! Введіть число.`)) : secondNumber;
+const thirdNumber = Number(prompt('Введіть третє число'));
+const checkNum3 = isNaN(thirdNumber) ? Number(prompt(`Помилка! Введіть число.`)) : thirdNumber;
 
-const arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+const arithmeticMean = (( checkNum1 || firstNumber) + (checkNum2 || secondNumber) + ( checkNum3 || thirdNumber)) / 3;
 
+console.log(arithmeticMean);
 alert(`Середнє арифметичне введених чисел: ${arithmeticMean}.`);
