2e9504025cb77016e12c34569ac1ec354117a547
LizaRebenok <lizabelichenko3@gmail.com>
2024-08-22 15:07:14 +0300

    HW 15 - completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 `Use strict`;
+
+function generateKey(length, characters) {
+  let lengthSpan = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+    lengthSpan += characters[index];
+  }
+  return lengthSpan;
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
