e373d3a82cfbbee0b52ab6b13ad568d5df7c3816
LizaRebenok <lizabelichenko3@gmail.com>
2024-09-09 20:11:02 +0300

    HW 23 - completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,39 @@
 `Use strict`;
+
+const balanceSymbolRemove = (string, symbolRemove = []) => {
+  if (typeof string !== 'string') throw new Error('String не рядок');
+
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (symbolRemove.includes(string[i])) continue;
+    result.push(string[i]);
+  }
+  return result.join('');
+};
+
+const phoneNumbersMinBalance = (
+  arr,
+  minBalance = 0,
+  usersKeys = {
+    balance: 'balance',
+    phone: 'phone',
+  },
+) => {
+  if (!Array.isArray(arr) || typeof minBalance !== 'number') return [];
+  const result = [];
+
+  arr.forEach((item) => {
+    if (minBalance <= 0) {
+      result.push(item[usersKeys.phone]);
+      return;
+    }
+    const balanceCorrected = Number(balanceSymbolRemove(item[usersKeys.balance], ['$', ',']));
+    if (balanceCorrected > minBalance) result.push(item[usersKeys.phone]);
+  });
+
+  return result;
+};
+
+const usersNumber = phoneNumbersMinBalance(users, 2000);
+console.log(usersNumber);
