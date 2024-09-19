0a7fc6fcd116e5b93a67af0003d9681c8cfa41ab
LizaRebenok <lizabelichenko3@gmail.com>
2024-09-20 00:33:58 +0300

    HW 28 - completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,25 @@
 `Use strict`;
 
-// Task #1
-
-const factorialNum = (num) => {
+// Task 1
+const getFactorial = (num) => {
   if (num === 0) {
     return 1;
   }
-  return num * factorialNum(num - 1);
+  return num * getFactorial(num - 1);
 };
-console.log(factorialNum(3));
+console.log(getFactorial(3));
+
+// Task 2
+const num = Number(prompt('Введіть число, яке потрібно звести в ступінь.'));
+const degree = Number(prompt('Введіть ступінь числа.'));
+const pow = (num, degree) => {
+  if (degree === 0) {
+    return 1;
+  }
+  return num * pow(num, degree - 1);
+};
+
+const resultPow = pow(num, degree);
+console.log(`Результат: ${num}^${degree} = ${resultPow}.`);
+
+// Task 3
