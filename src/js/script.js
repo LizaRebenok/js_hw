c83b42d4e8ddd6f75685abd753e9ee8a3b341794
LizaRebenok <lizabelichenko3@gmail.com>
2024-09-24 00:55:27 +0300

    HW 30 - Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 `Use strict`;
+
+const counter = () => {
+    let count = 0;
+
+    return (...args) => {
+        if (args.length !== 1) {
+            throw new Error(`Функція приймає тільки один аргумент.`);
+        }
+
+        const num = args[0];
+        if (typeof num !== 'number') {
+            throw new Error(`Фунція приймає в якості аргумента тільки число.`)
+        }
+        count += num;
+        return count;
+    };
+};
+
+const sum = counter();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(7));
+console.log(sum(20));
