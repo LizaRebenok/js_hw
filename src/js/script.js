f7f2f5f4646ca42186fa88e31f3b7e955cbfac50
LizaRebenok <lizabelichenko3@gmail.com>
2024-09-24 16:29:31 +0300

    HW 31 - Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
 `Use strict`;
+
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    },
+    reset: function() { // обнулення step
+        this.step = 0;
+        return this;
+    }
+};
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep();
+
+ladder.reset(); //виконання цього методу потрібно, щоб скинути до 0 step, аби виклик ланцюжка не враховував попередне значення.
+
+ladder.up().up().down().showStep();
