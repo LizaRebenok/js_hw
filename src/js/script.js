6f572f489104ed46f4e9afa1565fc2c0c23169df
LizaRebenok <lizabelichenko3@gmail.com>
2024-09-27 21:17:00 +0300

    HW 33 - Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 `Use strict`;
+
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+const randomImages = () => Math.floor(Math.random() * images.length);
+
+const getImage = images[randomImages()];
+
+const image = document.createElement('img');
+image.src = `images/${getImage}`;
+document.body.append(image);
