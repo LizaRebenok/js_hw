9a06bf22ee876d89bc1ec39d77c7972d5f8ba963
LizaRebenok <lizabelichenko3@gmail.com>
2024-10-03 16:22:23 +0300

    HW 33 - Fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
 `Use strict`;
 
-const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
-const randomImages = () => Math.floor(Math.random() * images.length);
+const IMG_FOLDER = './images/';
+const IMG_EXT = '.jpg';
 
-const getImage = images[randomImages()];
+const appendElement = (parentElement = null, elementToAppend = null) => {
+  if (!parentElement || !elementToAppend) return;
+  parentElement.append(elementToAppend);
+};
 
-const image = document.createElement('img');
-image.src = `images/${getImage}`;
-document.body.append(image);
+const getRandomNumberInRange = (min = 0, max = 10) => {
+  const num = Math.floor(Math.random() * (max - min));
+  if (num === 0) return 1;
+  return num;
+};
+
+const createImage = (imageName) => {
+  const img = document.createElement('img');
+  img.src = `${IMG_FOLDER}${imageName}${IMG_EXT}`;
+  img.alt = `${IMG_FOLDER}${imageName}${IMG_EXT}`;
+  img.title = `${IMG_FOLDER}${imageName}${IMG_EXT}`;
+  return img;
+};
+
+const randomImg = createImage(getRandomNumberInRange());
+appendElement(document.body, randomImg);
