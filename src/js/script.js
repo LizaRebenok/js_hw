a3450273c17f57b0df6a07c0c590b97ca6673f46
LizaRebenok <lizabelichenko3@gmail.com>
2024-10-04 17:30:18 +0300

    HW 34 - Completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 `Use strict`;
+
+const textField = document.getElementById('textField');
+
+const createDiv = () => {
+  const div = document.createElement('div');
+  div.classList.add('div');
+  document.body.append(div);
+};
+
+const deleteDiv = () => {
+  const div = document.querySelector('div');
+  if (div) {
+    div.remove();
+  }
+};
+
+textField.onmouseover = () => {
+  createDiv();
+};
+
+textField.onmouseout = () => {
+  deleteDiv();
+};
