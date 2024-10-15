cb9f3adf6451d317c7e8d6c3f34422fbbb85773a
LizaRebenok <lizabelichenko3@gmail.com>
2024-10-15 14:05:35 +0300

    HW 38 - Completed

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,22 @@
 import getFormHandlers from './formHandlers.js';
 import { createTodoItem } from './elements.js';
 import { prependElement } from './addElementToHTML.js';
+import { storageKey } from './const.js';
 
 (function () {
   const form = document.querySelector('[data-todo-form]');
   const todoItemsWrapper = document.querySelector('#todoItems');
 
+  const saveData = (data) => {
+    const savedData = JSON.parse(localStorage.getItem(storageKey)) || [];
+    savedData.push(data);
+    localStorage.setItem(storageKey, JSON.stringify(savedData));
+    console.log(savedData);
+  };
+
   const handleFormData = ({ data, event }) => {
     event.target.reset();
+    saveData(data);
     const todoItemElement = createTodoItem(data);
     prependElement(todoItemsWrapper, todoItemElement);
   };
@@ -18,4 +27,13 @@ import { prependElement } from './addElementToHTML.js';
 
   form.addEventListener('submit', submitHandler);
   form.addEventListener('input', inputHandler);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const savedData = JSON.parse(localStorage.getItem(storageKey));
+
+    savedData.forEach((todoItem) => {
+      const todoItemElement = createTodoItem(todoItem);
+      prependElement(todoItemsWrapper, todoItemElement);
+    });
+  });
 }());
