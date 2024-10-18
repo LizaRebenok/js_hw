61792ce92e29333350267d1f2faa86bf9fd33079
LizaRebenok <lizabelichenko3@gmail.com>
2024-10-18 16:29:58 +0300

    HW 38 - updated

--- src/js/script.js
+++ src/js/script.js
@@ -3,37 +3,37 @@
 import getFormHandlers from './formHandlers.js';
 import { createTodoItem } from './elements.js';
 import { prependElement } from './addElementToHTML.js';
-import { storageKey } from './const.js';
 
 (function () {
   const form = document.querySelector('[data-todo-form]');
   const todoItemsWrapper = document.querySelector('#todoItems');
 
-  const saveData = (data) => {
-    const savedData = JSON.parse(localStorage.getItem(storageKey)) || [];
-    savedData.push(data);
-    localStorage.setItem(storageKey, JSON.stringify(savedData));
-    console.log(savedData);
+  const saveTask = (data) => {
+    const saveDataForTask = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    saveDataForTask.push(data);
+    localStorage.setItem('tasks', JSON.stringify(saveDataForTask));
+    console.log(saveDataForTask);
   };
 
-  const handleFormData = ({ data, event }) => {
+  const handleFormData = ({ formData, event }) => {
     event.target.reset();
-    saveData(data);
-    const todoItemElement = createTodoItem(data);
+    saveTask(formData);
+    const todoItemElement = createTodoItem(formData);
     prependElement(todoItemsWrapper, todoItemElement);
+  };
+
+  const loadTasksOnThePage = () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach((todoItem) => {
+      const todoItemElement = createTodoItem(todoItem);
+      prependElement(todoItemsWrapper, todoItemElement);
+    });
   };
 
   const { submitHandler, inputHandler } = getFormHandlers(form, handleFormData);
 
   form.addEventListener('submit', submitHandler);
   form.addEventListener('input', inputHandler);
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const savedData = JSON.parse(localStorage.getItem(storageKey));
-
-    savedData.forEach((todoItem) => {
-      const todoItemElement = createTodoItem(todoItem);
-      prependElement(todoItemsWrapper, todoItemElement);
-    });
-  });
+  document.addEventListener('DOMContentLoaded', loadTasksOnThePage);
 }());
