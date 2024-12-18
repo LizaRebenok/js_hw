ce1473e741dd8f3e6db1ad7dca4c597197c3cf88
LizaRebenok <lizabelichenko3@gmail.com>
2024-12-19 01:27:23 +0200

    HW 47 - completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
 # Homework description 
+За допомогою ajax-запиту вивести погоду
+
+http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=eed2786d90d23080e7ba7aabd77527cd
+
+q=XXX - місто, для якого показати погоду
+temp – температура
+pressure - тиск
+description – опис
+humidity – вологість
+speed – швидкість вітру
+deg - напрям у градусах
+icon - значок, де 10d код іконки
+http://openweathermap.org/img/w/10d.png
---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+const weatherBlock = document.querySelector('#weather');
+
+const urlWeather = 'https://api.openweathermap.org/data/2.5/weather?q=DNIPRO&units=metric&APPID=5d066958a60d315387d9492393935c19';
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Помилка під час завантаження даних.');
+    }
+    const data = await response.json();
+    console.log(data);
+    getWeatherData(data);
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
+};
+
+const getWeatherData = (data) => {
+  const location = data.name;
+  const temp = Math.round(data.main.temp);
+  const { pressure } = data.main;
+  const description = data.weather[0].main;
+  const { humidity } = data.main;
+  const { speed } = data.wind;
+  const { deg } = data.wind;
+  const { icon } = data.weather[0];
+
+  const widget = `
+    <div class="weather-widget">
+      <div class="weather-city">${location}</div>
+      <div class="weather-temp">Температура: ${temp}°C</div>
+       <div class="weather-description">${description}</div>
+      <div class="weather-pressure">Тиск: ${pressure}</div>
+      <div class="weather-humidity">Вологість: ${humidity}%</div>
+      <div class="weather-speed">Швидкість вітру: ${speed} м/с</div>
+      <div class="weather-wind">Напрямок вітру: ${deg}°</div>
+      <div class="weather-icon">
+        <img src="http://openweathermap.org/img/w/${icon}.png" alt="${description}">
+      </div>
+    </div>
+  `;
+
+  weatherBlock.innerHTML = widget;
+};
+
+fetchData(urlWeather);
